632fcb8baacb5f079bf573a8b2f2fe8c6f1c5878
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-11-19 10:29:01 +0100

    Add project files.

---
+++ Dolgozo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek
+{
+    internal class Dolgozo
+    {
+        public string Nev {  get; set; }
+        public bool Neme { get; set; }
+        public string Reszleg { get; set; }
+        public string BelepesiEv { get; set; }
+        public int Ber {  get; set; }
+
+        public override string ToString()
+        {
+            return $"Név: {Nev}\nNeme: {(Neme ? "férfi" : "nő")}\nRészleg: {Reszleg}\nBelépés éve: {BelepesiEv}\nBér: {Ber}";
+        }
+
+        public Dolgozo(string sor) {
+            var temp = sor.Split(';');
+            Nev = temp[0];
+            Neme = temp[1] == "férfi";
+            Reszleg = temp[2];
+            BelepesiEv = temp[3];
+            Ber = int.Parse(temp[4]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Berek;
+using System.Linq;
+
+List<Dolgozo> dolgozok = new();
+
+using StreamReader sr = new(
+    path: @"../../../src/berek2020.txt",
+    encoding: System.Text.Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) dolgozok.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"3. feladat:\n\tA dolgozók száma: {dolgozok.Count()}");
+
+var f4 = dolgozok.Average(d => d.Ber);
+Console.WriteLine($"4. feladat:\n\tA dolgozók átlagbére 2020-ben: {f4/1000:0.0} ezer forint");
+
+Console.Write("\n5. feladt:\nAdjon meg egy részleget:  ");
+var f5 = Console.ReadLine();
+
+if(dolgozok.Where(d => d.Reszleg.Equals(f5)).Count() != 0)
+{
+    var f6 = dolgozok.Where(d => d.Reszleg.Equals(f5)).OrderByDescending(d => d.Ber).First();
+    Console.WriteLine($"\n6. feladat:\n{f6.ToString()}");
+}
+else Console.WriteLine($"\n6. feladat:\n\t{"Nincs ilyen részleg"}");
+
+var f7 = dolgozok
+    .GroupBy(d => d.Reszleg)
+    .ToDictionary(d => d.Key, d => d.Count())
+    .OrderBy(d => d.Key);
+Console.WriteLine("\n7. feladat:");
+foreach (var f in f7) Console.WriteLine($"\t{f.Key,11}: {f.Value}");
